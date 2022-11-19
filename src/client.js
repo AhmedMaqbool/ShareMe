89a38a55e9c53383774012b9d2a832038ec45e04
Ahmed Maqbool <ahmedmaqx11@gmail.com>
2022-11-19 15:14:07 +0300

    inital commit

---
+++ src/client.js
@@ -0,0 +1,15 @@
+import sanityClient from "@sanity/client";
+import imageBuilder from "@sanity/image-url";
+
+
+export const client = sanityClient({
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    dataset: "production",
+    apiVersion: "2022-10-31",
+    useCdn: true,
+    token: process.env.REACT_APP_SANITY_TOKEN
+})
+
+const builder = imageBuilder(client)
+
+export const urlFor = (source) => builder.image(source)
